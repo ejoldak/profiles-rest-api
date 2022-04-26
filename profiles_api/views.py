2f9c13e5c0e00b83645d314f94d01ad76382114a
Edziu_ <ejoldak@gmail.com>
2022-04-26 18:24:43 -0400

    all set

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,7 +6,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 from profiles_api import serializers
 from profiles_api import models
@@ -111,13 +111,11 @@ class UserLoginAPIView(ObtainAuthToken):
 
 class UserProfileFeedViewSet(viewsets.ModelViewSet):
     """Handles creating, reading, and updating profile feed items"""
-    authentication_classes = (TokenAuthentication, )
+    authentication_classes = (TokenAuthentication,)
     serializer_class = serializers.ProfileFeedItemSerializer
     queryset = models.ProfileFeedItem.objects.all()
     permission_classes = (
-        permissions.UpdateOwnStatus,
-        IsAuthenticatedOrReadOnly   
-    )
+        permissions.UpdateOwnStatus,IsAuthenticated)
 
     def perform_create(self, serializer):
         """Sets the user profile to the logged-in user"""
